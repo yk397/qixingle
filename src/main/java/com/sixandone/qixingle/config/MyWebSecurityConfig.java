60b67d96d6e9c9538e1ff5a8cefc5cbad3011744
YK <3558836039@qq.com>
2023-05-05 20:51:20 +0800

    登录功能2

---
+++ src/main/java/com/sixandone/qixingle/config/MyWebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.sixandone.qixingle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName yk
+ * @Descprition:接口安全配置
+ * @Autor DELL
+ * @Date 2023/5/5 14:07
+ **/
+@Configuration
+public class MyWebSecurityConfig {
+
+    @Bean
+    RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new WxMappingJackson2HttpMessageConverter());
+        return restTemplate;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+
+
+        return (web -> web.ignoring().antMatchers("/userLogin/**"));
+    }
+
+}
---
+++ src/main/java/com/sixandone/qixingle/vo/reciveLoginInfo.java
@@ -0,0 +1,17 @@
+package com.sixandone.qixingle.vo;
+
+import lombok.Data;
+
+/**
+ * @ClassName yk
+ * @Descprition:接收登录参数对象
+ * @Autor DELL
+ * @Date 2023/5/5 12:16
+ **/
+@Data
+public class reciveLoginInfo {
+    private String jsCode;
+    private String userName;
+    private String phoneNumber;
+
+}
